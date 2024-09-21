12d1d9b8d1ac56fbadc9547666c64b3dc1753bf5
zubair7384 <zubair.iqbal2018@gmail.com>
2024-09-21 22:02:38 +0000

    Integrated the Inter Font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)',
-  			primary: '#0F243D',
-  			secondary: '#4183F5',
-  			primaryTransparent: '#0F243DCC',
-  			accent: '#3C82F6',
-  			lightGray: '#1C324D',
-  			lightBlue: '#0C6FE0'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)"],
+        mono: ["var(--font-roboto-mono)"],
+      },
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        primary: "#0F243D",
+        secondary: "#4183F5",
+        primaryTransparent: "#0F243DCC",
+        accent: "#3C82F6",
+        lightGray: "#1C324D",
+        lightBlue: "#0C6FE0",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
