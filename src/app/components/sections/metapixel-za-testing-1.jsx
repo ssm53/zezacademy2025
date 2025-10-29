347177b4d6e6a1affd989990590d7ec26b369217
Shaun Shanil Menezes <shaunshanil95@gmail.com>
2025-10-29 12:44:12 +0800

    did meta pixel za-testing-1

---
+++ src/app/components/sections/metapixel-za-testing-1.jsx
@@ -0,0 +1,53 @@
+// za-testing-1 pixel code
+// <!-- Meta Pixel Code -->
+// <script>
+// !function(f,b,e,v,n,t,s)
+// {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+// n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+// if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+// n.queue=[];t=b.createElement(e);t.async=!0;
+// t.src=v;s=b.getElementsByTagName(e)[0];
+// s.parentNode.insertBefore(t,s)}(window, document,'script',
+// 'https://connect.facebook.net/en_US/fbevents.js');
+// fbq('init', '829840750216936');
+// fbq('track', 'PageView');
+// </script>
+// <noscript><img height="1" width="1" style="display:none"
+// src="https://www.facebook.com/tr?id=829840750216936&ev=PageView&noscript=1"
+// /></noscript>
+// <!-- End Meta Pixel Code -->
+
+
+import Image from "next/image";
+import Script from "next/script";
+import React from "react";
+
+const MetaPixelZaTesting1 = () => {
+  return (
+    <> 
+   
+<Script strategy="afterInteractive" id="facebook-pixel" dangerouslySetInnerHTML={{
+    __html: `
+    !function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '829840750216936');
+fbq('track', 'PageView');
+    `
+}}/>
+<noscript><Image height={1} width={1} style={{display: "none"}}
+src="https://www.facebook.com/tr?id=829840750216936&ev=PageView&noscript=1"
+/></noscript>
+
+  
+      
+    </>
+  );
+};
+
+export default MetaPixelZaTesting1;
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css"; // Import global styles
 import LenisScroll from "../lenis-scroll"; // Import the Lenis scroll client component
+import dynamic from "next/dynamic";
+const MetaPixel = dynamic(import("./components/sections/metapixel-za-testing-1")) // meta pixel
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,9 +25,30 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.variable} font-sans`}>
         <LenisScroll /> {/* Include the Lenis smooth scroll */}
+        {/* meta pixel */}
+        <MetaPixel/> 
         {children}
       </body>
     </html>
   );
 }
-//
+
+
+// za-testing-1 pixel code
+// <!-- Meta Pixel Code -->
+// <script>
+// !function(f,b,e,v,n,t,s)
+// {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+// n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+// if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+// n.queue=[];t=b.createElement(e);t.async=!0;
+// t.src=v;s=b.getElementsByTagName(e)[0];
+// s.parentNode.insertBefore(t,s)}(window, document,'script',
+// 'https://connect.facebook.net/en_US/fbevents.js');
+// fbq('init', '829840750216936');
+// fbq('track', 'PageView');
+// </script>
+// <noscript><img height="1" width="1" style="display:none"
+// src="https://www.facebook.com/tr?id=829840750216936&ev=PageView&noscript=1"
+// /></noscript>
+// <!-- End Meta Pixel Code -->
